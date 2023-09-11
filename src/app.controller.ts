5d68c2c0351ce01a932a0354f00eaaa0c69bf322
arch-ville <levon.ter-minasyan@wunderdog.fi>
2023-09-11 18:30:52 +0200

    test2

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,6 +5,7 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
   test = "";
+  test2 = ""
   @Get()
   getHello(): string {
     return this.appService.getHello();
